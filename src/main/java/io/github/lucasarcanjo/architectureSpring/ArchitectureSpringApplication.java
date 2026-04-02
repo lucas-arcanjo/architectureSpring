42494261b58039e08b16d189ee1eb1a71f2a90c9
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2026-04-01 22:10:03 -0300

    feat: project

---
+++ src/main/java/io/github/lucasarcanjo/architectureSpring/ArchitectureSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucasarcanjo.architectureSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureSpringApplication.class, args);
+	}
+
+}
